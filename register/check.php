efbabfa085839c1a3a0e706a128ba0708552907d
SasanoHayato <sasano.hayato@gmail.com>
2020-06-28 20:26:36 +0800

    Set template check.php

---
+++ register/check.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Learn SNS</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
+</head>
+<body>
+    <script src="../assets/js/jquery-3.1.1.js"></script>
+    <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
+    <script src="../assets/js/bootstrap.js"></script>
+</body>
+</html>
