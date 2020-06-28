eac73706f597cbf15f520fc31ae74d125e3729ca
SasanoHayato <sasano.hayato@gmail.com>
2020-06-28 23:41:41 +0800

    write down form

--- signin.php
+++ signin.php
@@ -1,19 +1,52 @@
 <?php
+    // 初期化
+    $errors = [];
+    if (!empty($_POST)) {
+      // ①
+      $email = $_POST['input_email'];
+      $password = $_POST['input_password'];
+      if ($email != '' && $password != '') {
+          // データベースとの照合処理
+      } else {
+          $errors['signin'] = 'blank';
+      }
+    }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="utf-8">
     <title>Learn SNS</title>
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="./assets/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
 </head>
 <body style="margin-top: 60px">
-    
+    <div class="container">
+      <div class="row">
+          <div class="col-xs-8 col-xs-offset-2 thumbnail">
+              <h2 class="text-center content_header">サインイン</h2>
+              <form method="POST" action="signin.php">
+                  <div class="form-group">
+                    <label for="email">メールアドレス</label>
+                    <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
+                    <?php if(isset($errors['signin']) && $errors['signin'] == 'blank'): ?>
+                      <p class="text-danger">メールアドレスとパスワードを正しく入力してください</p>
+                    <?php endif; ?>
+                  </div>
+                  <div class="form-group">
+                    <label for="password">パスワード</label>
+                    <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
+                  </div>
+                  <input type="submit" class="btn btn-info" value="サインイン">
+              </form>
+          </div>
+      </div>
+    </div>
 </body>
-<script src="../assets/js/jquery-3.1.1.js"></script>
-<script src="../assets/js/jquery-migrate-1.4.1.js"></script>
-<script src="../assets/js/bootstrap.js"></script>
+<script src="./assets/js/jquery-3.1.1.js"></script>
+<script src="./assets/js/jquery-migrate-1.4.1.js"></script>
+<script src="./assets/js/bootstrap.js"></script>
 </html>
